a74f73852ddb8ac8f1d45ad0f1a97eb8b6ab13f2
Joel Nider <joel@nider.org>
2021-02-10 19:30:19 -0800

    Set up a swap device
    Signed-off-by: Joel Nider <joel@nider.org>

--- pim_swap.c
+++ pim_swap.c
@@ -1,18 +1,82 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <linux/module.h>
+#include <linux/frontswap.h>
+
+static char *dpu_mem;
+
+static void pimswap_frontswap_init(unsigned type)
+{
+	pr_err("%s: \n", __func__);
+}
+
+/* attempts to compress and store an single page */
+static int pimswap_frontswap_store(unsigned type, pgoff_t offset,
+				struct page *page)
+{
+	pr_err("%s: type=%u offset=%u\n", __func__, type, offset);
+
+	/* THP isn't supported */
+	if (PageTransHuge(page)) {
+		pr_err("%s: can't cache hugepages\n", __func__);
+		return -EINVAL;
+	}
+	return 0;
+}
+
+/** Load a compressed page (swap in)
+ * returns 0 if the page was successfully decompressed
+ * return -1 on entry not found or error
+*/
+static int pimswap_frontswap_load(unsigned type, pgoff_t offset,
+				struct page *page)
+{
+	pr_err("%s: \n", __func__);
+	return -1;
+}
+
+/* frees an entry in zswap */
+static void pimswap_frontswap_invalidate_page(unsigned type, pgoff_t offset)
+{
+	pr_err("%s: \n", __func__);
+}
+
+/* frees all zswap entries for the given swap type */
+static void pimswap_frontswap_invalidate_area(unsigned type)
+{
+	pr_err("%s: \n", __func__);
+}
+
+static struct frontswap_ops pimswap_frontswap_ops = {
+	.store = pimswap_frontswap_store,
+	.load = pimswap_frontswap_load,
+	.invalidate_page = pimswap_frontswap_invalidate_page,
+	.invalidate_area = pimswap_frontswap_invalidate_area,
+	.init = pimswap_frontswap_init
+};
 
 static int __init init_pim_swap(void)
 {
-	printk("%s: hello!", __func__);
-	pr_err("init ok\n");
+	pr_err("%s: started\n", __func__);
 
+	// grab some memory to simulate the DPUs
+	dpu_mem = kmalloc(1024 * 1024, GFP_KERNEL);
+	if (!dpu_mem) {
+		pr_err("%s: can't alloc DPU memory\n", __func__);
+		return -1;
+	}
+
+	frontswap_register_ops(&pimswap_frontswap_ops);
+
+	pr_err("%s: done\n", __func__);
 	return 0;
 }
 
 static void __exit exit_pim_swap(void)
 {
-	printk("%s: exit\n", __func__);
+	pr_info("%s: exit\n", __func__);
+
+	kfree(dpu_mem);
 }
 
 module_init(init_pim_swap);
