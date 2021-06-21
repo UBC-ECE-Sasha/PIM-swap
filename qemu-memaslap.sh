1b286cc9b1f6d61e4c9a52a29c0c8c7fbfb1cf70
Jackson Dagger <jacksonddagger@gmail.com>
2021-06-21 16:49:56 -0700

    setting up qemu+memcached+memalsap scripts

---
+++ qemu-memaslap.sh
@@ -0,0 +1,19 @@
+# run memcached on qemu and memaslap locally
+CORES="${1:-4}" # number of cores to emulate
+QEMU_MEM="${2:-2400}" # memory in MB of QEMU guest
+MEMCACHED_MEM="${3:-1600}" # memory in MB limit for memcached
+SCRIPT="memcached -u nobody -m ${MEMCACHED_MEM}"
+
+qemu-system-x86_64 -enable-kvm \
+ -kernel output/images/bzImage \
+ -initrd output/images/rootfs.cpio.gz \
+ -append "console=ttyS0" \
+ -cpu host \
+ -smp $CORES \
+ -m $QEMU_MEM \
+ -usb \
+ -chardev pty,id=ser0 \
+ -serial chardev:ser0 \
+ -net user,hostfwd=tcp::10022-:22,hostfwd=tcp::11212-:11211 -net nic \
+ -nographic \
+ -drive file=swap-1g.raw,format=raw,if=ide 
