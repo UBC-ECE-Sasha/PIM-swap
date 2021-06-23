20083d7e058f4593ad6a067768f5335f1eb38a84
Jackson Dagger <jacksonddagger@gmail.com>
2021-06-23 15:31:34 -0700

    working on getting ssh running

--- memcached_test.py
+++ memcached_test.py
@@ -2,7 +2,7 @@ import subprocess
 
 def memaslap_test(qemu_cores, qemu_mem, memcached_mem):
     logfile = open("memaslap.log", "w")
-    qemu_cmd = "qemu-system-x86_64 -enable-kvm " +\
+    qemu_cmd = "exec qemu-system-x86_64 -enable-kvm " +\
         "-kernel output/images/bzImage " +\
         "-initrd output/images/rootfs.cpio.gz " +\
         "-append \"console=ttyS0\" " +\
@@ -17,26 +17,30 @@ def memaslap_test(qemu_cores, qemu_mem, memcached_mem):
         "-drive file=swap-1g.raw,format=raw,if=ide"
 
     print("starting QEMU")
+    # start buildroot on QEMU
     qemu_instance = subprocess.Popen(qemu_cmd, stdout=subprocess.PIPE, shell=True)
     try:
-        qemu_out, qemu_err = qemu_instance.communicate(timeout = 5)
+        qemu_out, qemu_err = qemu_instance.communicate(timeout = 5) # timeout gives QEMU time to start
         raise SystemError("QEMU closed unexpectedly with STDOUT:\n" + str(qemu_out) + "\nSTDERR:\n" + str(qemu_err))
     except subprocess.TimeoutExpired:
         pass
 
     print("sshing into QEMU")
-    ssh_cmd = "sshpass -p \"root\" " +\
+    # ssh into QEMU and start memcached
+    ssh_cmd = "exec sshpass -p \"root\" " +\
         "ssh root@localhost " +\
         "-p 10022 " +\
         "-o \"UserKnownHostsFile /dev/null\" " +\
         "-o StrictHostKeyChecking=no " +\
-        "\"exec memcached -u nobody -m " + str(memcached_mem) + "\""
+        "\"" + "memcached --daemon -u nobody -m " + str(memcached_mem) + "\""
 
-    ssh = subprocess.Popen(ssh_cmd, stdout=subprocess.PIPE, shell=True)
+    ssh = subprocess.Popen(ssh_cmd, shell=True)
 
     memaslap_cmd = "memaslap -s localhost:11212 –facebook –division=50"
+
+    # run memaslap to prepopulate the server
     print("first memaslap run")
-    memaslap = subprocess.Popen(memaslap_cmd, stdout=subprocess.PIPE, shell=True)
+    memaslap = subprocess.Popen(memaslap_cmd, stdout=logfile, shell=True)
 
     try:
         ms_out, ms_err = memaslap.communicate(timeout = 10*60 + 5)
@@ -44,7 +48,8 @@ def memaslap_test(qemu_cores, qemu_mem, memcached_mem):
         raise SystemError("Memaslap didn't close after 10m")
 
     print("second memaslap run")
-    memaslap = subprocess.Popen(memaslap_cmd, stdout=logfile, shell=True)
+    # run memaslap to collect data
+    memaslap = subprocess.Popen(memaslap_cmd, stdout=subprocess.PIPE, shell=True)
     try:
         ms_out, ms_err = memaslap.communicate(timeout = 10*60 + 5)
     except subprocess.TimeoutExpired:
