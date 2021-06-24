839a0aeff56d8051e5f12dc70089dfd246ef7118
Jackson Dagger <jacksonddagger@gmail.com>
2021-06-23 17:24:46 -0700

    benchmark script trying to debug ssh

--- qemu-memaslap.sh
+++ qemu-memaslap.sh
@@ -16,4 +16,11 @@ qemu-system-x86_64 -enable-kvm \
  -serial chardev:ser0 \
  -net user,hostfwd=tcp::10022-:22,hostfwd=tcp::11212-:11211 -net nic \
  -nographic \
- -drive file=swap-1g.raw,format=raw,if=ide 
+ -drive file=swap-1g.raw,format=raw,if=ide &
+
+ sleep 10 && sshpass -p "root" \
+  ssh root@localhost \
+  -p 10022 \
+  -o "UserKnownHostsFile /dev/null"  +\
+  -o StrictHostKeyChecking=no \
+  "memcached --daemon -u nobody -m $MEMCACHED_MEM"
