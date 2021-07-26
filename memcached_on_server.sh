e4afa8cd0b78c4f689510fdce7bf2ffe793883f1
Jackson Dagger <jacksonddagger@gmail.com>
2021-07-26 09:09:30 -0700

    benchmarking folder

--- memcached_on_server.sh
+++
@@ -1,8 +0,0 @@
-$SCRIPT="memcached -u nobody -m " "$MEMCACHED_MEM"
-
-sshpass -p "root" \ # we don't use a more secure option because the password is "root"
-  ssh root@localhost \
-  -p 10022 \
-  -o "UserKnownHostsFile /dev/null" \
-  -o StrictHostKeyChecking=no \
-  "bash -s" < memcached_on_server.sh
