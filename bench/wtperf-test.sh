de28fac2cecae1cb8a89c9ed5328b278d7947998
Jackson Dagger <JacksonDDagger@gmail.com>
2022-03-25 09:13:23 +0100

    seperate logging into own script

--- bench/wtperf-test.sh
+++ bench/wtperf-test.sh
@@ -14,14 +14,9 @@ LOG_DIR="logs/WT_${CONFIG_BASENAME}_${EXTRA_LOG_NAME}$(date '+%Y-%m-%d--%H-%M-%S
 mkdir $LOG_DIR
 echo $LOG_DIR
 
-vmstat -n -t 1 > $LOG_DIR/vmstat.log 2>&1 &
-MEMLOG_PID=$!
-
-perf record -e page-faults -o $LOG_DIR/perf.data $WTPERF_LOC/wtperf -O $CONFIG -h $DB_DIR > $LOG_DIR/wt_out.log 2>&1 &
+$WTPERF_LOC/wtperf -O $CONFIG -h $DB_DIR > $LOG_DIR/wt_out.log 2>&1 &
 WTPERF_PID=$!
-
-pidstat -p $WTPERF_PID -r -u 1 > $LOG_DIR/pidstat.log 2>&1
-kill $MEMLOG_PID
+./process-log.sh $WTPERF_PID $LOG_DIR
 
 cp $DB_DIR/monitor.json $LOG_DIR/monitor.json
 cp $DB_DIR/test.stat $LOG_DIR/test.stat
