fc88fc03223a05587713734fd70ccf4df8bb4090
Jackson Dagger <JacksonDDagger@gmail.com>
2022-04-01 05:15:29 +0200

    update wtperf configs

--- bench/wtperf-series.sh
+++ bench/wtperf-series.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-sudo ./limit-mem.sh -m 8192 -d /scratch/ramdisk
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_22G.wtperf
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_14G.wtperf
+sudo ./limit-mem.sh -m 16384 -d /scratch/ramdisk
+./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_8G.wtperf
 ./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_16G.wtperf
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_18G.wtperf
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_20G.wtperf
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_22G.wtperf
+./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_24G.wtperf
+./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_32G.wtperf
+
+./wtperf-test.sh conf/wiredtiger/configs/ycsb-c_8G.wtperf
+./wtperf-test.sh conf/wiredtiger/configs/ycsb-c_16G.wtperf
+./wtperf-test.sh conf/wiredtiger/configs/ycsb-c_24G.wtperf
+./wtperf-test.sh conf/wiredtiger/configs/ycsb-c_32G.wtperf
