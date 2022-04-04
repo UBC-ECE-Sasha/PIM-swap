0456e53d16cf87df75b41e142966733fb004ecd7
Jackson Dagger <JacksonDDagger@gmail.com>
2022-04-04 21:35:12 +0200

    start recording minor and major faults with perf

--- bench/process-log.sh
+++ bench/process-log.sh
@@ -10,6 +10,6 @@ LOG_DIR=$2
 
 vmstat -n -t 1 > $LOG_DIR/vmstat.log 2>&1 &
 MEMLOG_PID=$!
-perf record -e page-faults -o $LOG_DIR/perf.data > $LOG_DIR/perf.log 2>&1 &
+perf record -e major-faults -e minor-faults -o $LOG_DIR/perf.data > $LOG_DIR/perf.log 2>&1 &
 pidstat -p $PID -r -u 1 > $LOG_DIR/pidstat.log 2>&1
 kill $MEMLOG_PID
