3135faf6104cfe35e3f9559e7a4034c5f30cdab8
Jackson Dagger <jacksonddagger@gmail.com>
2022-04-12 20:07:53 -0700

    update process-log.sh to save system state

--- bench/process-log.sh
+++ bench/process-log.sh
@@ -9,6 +9,16 @@
 PID=$1
 LOG_DIR=$2
 
+SYS_STATE_LOG=${LOG_DIR}/start.log
+echo "System state at start of log: $(date)" > ${SYS_STATE_LOG}
+echo "/proc/meminfo" >> ${SYS_STATE_LOG}
+cat /proc/meminfo >> ${SYS_STATE_LOG}
+echo "/proc/cpuinfo" >> ${SYS_STATE_LOG}
+cat /proc/cpuinfo >> ${SYS_STATE_LOG}
+echo "zswap" >> ${SYS_STATE_LOG}
+grep -R . /sys/module/zswap/parameters >> ${SYS_STATE_LOG}
+
+
 vmstat -n -t 1 > $LOG_DIR/vmstat.log 2>&1 &
 MEMLOG_PID=$!
 perf record -e major-faults -e minor-faults -o $LOG_DIR/perf.data > $LOG_DIR/perf.log 2>&1 &
