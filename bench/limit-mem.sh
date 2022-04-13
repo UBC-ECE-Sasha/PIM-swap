2bf2112f4435345126d05820c980846dd88f08a6
Jackson Dagger <jacksonddagger@gmail.com>
2022-04-13 11:34:22 -0700

    update limit-mem to be less confusing and restore local=test

--- bench/limit-mem.sh
+++ bench/limit-mem.sh
@@ -6,12 +6,9 @@
 # Script assumes ramfs is already mounted unless -r is specified.
 
 RAMFS_DIR=/mnt/ramdisk
-RAMFS_NUM=4
-RAMFS_DENOM=5
-RAMFS_SIZE=$((RAMFS_NUM*$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')/RAMFS_DENOM/1024/1024))
+RAMFS_SIZE=0
 MEMLEFT=0
 PREV_MEM_AVAIL=4096
-CMD_MOUNT="true"
 CMD_UNMOUNT="false"
 CMD_CLEAR="false"
 
@@ -20,19 +17,17 @@ print_usage() {
   echo "-m: specify memory limit in MB"
   echo "-s: specify size of ramfs in MB (default will be slightly bigger than needed)"
   echo "-c: clear ramfs"
-  echo "-r: don't mount ramdisk"
   echo "-u: unmount ramdisk"
   echo "-h: print this message"
   echo "read more at https://wiki.ubc.ca/PIM-SWAP"
 }
 
-while getopts 'd:m:p:s:cruh' flag; do
+while getopts 'd:m:p:s:cuh' flag; do
   case "${flag}" in
     d) RAMFS_DIR="${OPTARG}" ;;
     m) MEMLEFT="${OPTARG}" ;;
     s) RAMFS_SIZE="${OPTARG}" ;;
     c) CMD_CLEAR="true" ;;
-    r) CMD_MOUNT="false" ;;
     u) CMD_UNMOUNT="true" ;;
     h) print_usage
        exit 1 ;;
@@ -40,6 +35,22 @@ while getopts 'd:m:p:s:cruh' flag; do
        exit 1 ;;
   esac
 done
+
+TOTAL_MEM_MB=$(($(cat /proc/meminfo | grep MemTotal | awk '{print $2}')/1024))
+if [ "$TOTAL_MEM_MB" -lt "$MEMLEFT" ]; then
+  echo "Error: Memory limit is greater than total memory."
+  exit 1
+fi
+
+if [ "$RAMFS_SIZE" -le 0 ]; then
+    if [ "$TOTAL_MEM_MB" -gt 4096 ]; then
+        RAMFS_SIZE=$((TOTAL_MEM_MB - 2048))
+    else
+        RAMFS_NUM=3
+        RAMFS_DENOM=4
+        RAMFS_SIZE=$((RAMFS_NUM*TOTAL_MEM_MB/RAMFS_DENOM))
+    fi
+fi
 
 TESTFILE=${RAMFS_DIR}/testfile
 MEM_AVAIL_KB=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}')
@@ -64,12 +75,11 @@ specify_memory () {
             fill_ramdisk $FILL_MB
             sleep 2
         fi
-        cat /proc/meminfo
     fi
 }
 
 mount_ramdisk () {
-    mount -t ramfs -o size=${RAMFS_SIZE}G ext4 ${RAMFS_DIR}
+    mount -t ramfs -o size=${RAMFS_SIZE}M ext4 ${RAMFS_DIR}
 }
 
 umount_ramdisk () {
@@ -82,13 +92,12 @@ fi
 
 if [ $CMD_UNMOUNT == "true" ]; then
     umount_ramdisk
-
-    if [ "$CMD_MOUNT" != "true" ]; then
-        MEMLEFT=0
-    fi
 fi
 
-if [ $CMD_MOUNT == "true" ]; then
+FOUND_RAMFS_DIR=$(mount | grep ramfs | awk '{print $3}')
+if [ -z "$FOUND_RAMFS_DIR" ]; then
+    mount_ramdisk
+elif [ "$FOUND_RAMFS_DIR" != *"$RAMFS_DIR"* ]; then
     mount_ramdisk
 fi
 
