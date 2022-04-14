d8ffeb0ba5270fae196a446053d59a5c71d2955d
Jackson Dagger <JacksonDDagger@gmail.com>
2022-04-14 08:22:43 +0200

    update to mount with sudo

--- bench/wtperf-series.sh
+++ bench/wtperf-series.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-sudo ./limit-mem.sh -m 16384 -d /scratch/ramdisk
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_8G.wtperf
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_16G.wtperf
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_24G.wtperf
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-a_32G.wtperf
-
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-c_8G.wtperf
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-c_16G.wtperf
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-c_24G.wtperf
-./wtperf-test.sh conf/wiredtiger/configs/ycsb-c_32G.wtperf
+./local-test.sh -m 49152 -c conf/wiredtiger/configs/ycsb-c_24G.wtperf
+./local-test.sh -m 40960 -c conf/wiredtiger/configs/ycsb-c_24G.wtperf
+./local-test.sh -m 36864 -c conf/wiredtiger/configs/ycsb-c_24G.wtperf
+./local-test.sh -m 32768 -c conf/wiredtiger/configs/ycsb-c_24G.wtperf
+./local-test.sh -m 28672 -c conf/wiredtiger/configs/ycsb-c_24G.wtperf
+./local-test.sh -m 24576 -c conf/wiredtiger/configs/ycsb-c_24G.wtperf
+./local-test.sh -m 20480 -c conf/wiredtiger/configs/ycsb-c_24G.wtperf
